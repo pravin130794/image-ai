2b69d3c5b2447c6603833772b7b035c91c23cbf7
PR <anku130794@gmail.com>
2024-06-16 17:09:40 +0530

    added new components

--- components/shared/Sidebar.tsx
+++ components/shared/Sidebar.tsx
@@ -56,7 +56,27 @@ const Sidebar = () => {
               {navLinks.slice(6).map((link) => {
                 const isActive = link.route === pathname;
 
-                return <li key={link.route}></li>;
+                return (
+                  <li
+                    key={link.route}
+                    className={`sidebar-nav_element group ${
+                      isActive
+                        ? "bg-purple-gradient text-white"
+                        : "text-gray-700"
+                    }`}
+                  >
+                    <Link className="sidebar-link" href={link.route}>
+                      <Image
+                        src={link.icon}
+                        alt="logo"
+                        width={24}
+                        height={24}
+                        className={`${isActive && "brightness-200"}`}
+                      />
+                      {link.label}
+                    </Link>
+                  </li>
+                );
               })}
 
               <li className="flex-center cursor-pointer gap-2 p-4">
@@ -64,6 +84,7 @@ const Sidebar = () => {
               </li>
             </ul>
           </SignedIn>
+
           <SignedOut>
             <Button asChild className="button bg-purple-gradient bg-cover">
               <Link href="/sign-in">Login</Link>
